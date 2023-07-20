b1e40785a178112bcafc9a91324c0a0d0fc09775
mbaneshi <mbaneshi@users.noreply.github.com>
2023-07-20 22:35:04 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="mbaneshi"
+original_name="fpt"
+original_urlname="fpt"
+original_description="Awesome fpt created by mbaneshi"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv fpt $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- fpt/app.py
+++ fpt/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+fpt API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="fpt",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://fpt.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "mbaneshi",
+        "url": "http://fpt.com/contact/",
+        "email": "mbaneshi@fpt.com",
     },
     license_info={
         "name": "The Unlicense",
